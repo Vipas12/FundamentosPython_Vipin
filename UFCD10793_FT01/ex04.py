654cf9556d9e295c16919c798db3316f8b6699d6
Vipas12 <avipconta@gmail.com>
2025-02-02 21:03:14 +0000

    er

--- UFCD10793_FT01/ex04.py
+++ UFCD10793_FT01/ex04.py
@@ -1,7 +1,13 @@
-import math
+from math import pi
 raio = float(input("Escreva o raio da circunferencia: "))
 r= raio**3
 a=float(4/3)
-pi=float(3,14)
-volume = r*a*pi
-print (volume)
+PII=3.14
+print (type(r))
+print (type(PII))
+print (type(a))
+print (type(pi))
+volume = r*a*PII
+volume1 = r*a*pi
+print ("o volume de uma circunferencia de raio", raio, "é de", volume1 )
+print (volume1)
