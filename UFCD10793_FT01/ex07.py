ce3cd94dfbdec7f9ad61be7f17fd66423635006c
Vipas12 <avipconta@gmail.com>
2025-02-02 21:05:52 +0000

    df

--- UFCD10793_FT01/ex07.py
+++ UFCD10793_FT01/ex07.py
@@ -1,4 +1,4 @@
-kms = int(input("insira nº kms percorridos"))
-litros= int(input("insira n.º litros consumidos"))
+kms = int(input("insira nº kms percorridos: "))
+litros= int(input("insira n.º litros consumidos: "))
 consumo = kms / litros
 print("Por litro faz", consumo, "kms")
