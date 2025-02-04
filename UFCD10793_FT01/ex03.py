8f75eb7baad3bebcedd847c6edf31590adde5277
Vipas12 <avipconta@gmail.com>
2025-02-04 21:36:33 +0000

    er

--- UFCD10793_FT01/ex03.py
+++ UFCD10793_FT01/ex03.py
@@ -1,3 +1,7 @@
 num = int(input("Escreva um numero inteiro: "))
 print ("o dobro de", num , "é", num * 2)
+#output = "O dobro de %s é %s"%(numero,dobro)  #%s para string
+#output2 = "O dobro de {} é {}".format(numero,2*numero)
+#output3 = f"O dobro de {numero} é {dobro}" #formatted strings
+#print(f"O dobro de {numero} é {dobro}")  # Output: O dobro do
 
--- UFCD10793_FT01/ex06.py
+++ UFCD10793_FT01/ex06.py
@@ -5,3 +5,7 @@ horat = hora*60*60
 minutost=minutos*60
 print(hora*60*60 + minutos*60 + segundos)
 print(horat+minutost+segundos)
+"""segundos = horas * 3600 + minutos * 60 + segundos
+output = f"O total de segundos é: {segundos}"
+print(output)
+"""
