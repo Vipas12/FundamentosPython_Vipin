7e75089e21c61b5fd117c672d7de9da581cfa325
Vipas12 <avipconta@gmail.com>
2025-02-04 22:45:13 +0000

    er

---
+++ UFCD10793_FT03/ex03_02.py
@@ -0,0 +1,6 @@
+num1 = float(input("digite o 1º numero inteiro: "))
+num2 = float(input("digite o 2º numero inteiro: "))
+if num1 == num2:
+    print ("os numeros são iguais")
+else:
+    print ("os numeros são diferentes")
