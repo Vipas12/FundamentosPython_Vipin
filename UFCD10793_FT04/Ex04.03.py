3055e615aced2727218440488cb29f51548b42c9
Vipas12 <avipconta@gmail.com>
2025-02-08 17:31:45 +0000

    re

---
+++ UFCD10793_FT04/Ex04.03.py
@@ -0,0 +1,9 @@
+num1=int(input("digite o 1º numero: "))
+num2=int(input("digite o 2º numero: "))
+num3=int(input("digite o 3º numero: "))
+num4=int(input("digite o 4º numero: "))
+if num1>0 and num2>0 and num3>0 and num4>0:
+    print ("a média dos 4 numeros é", (num1+num2+num3+num4)/4)
+else:
+    print ("pelo menos 1 dos numeros são é inteiro.")
+
---
+++ UFCD10793_FT04/Ex04.05.py
@@ -0,0 +1,9 @@
+n=0
+while n < 10:
+    n=n+1
+    print (n)
+    if (n==10):
+        break
+else:
+    print("n é maior que 5")
+
