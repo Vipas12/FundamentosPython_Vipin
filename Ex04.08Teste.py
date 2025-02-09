7b58afe916c8e85e34b0cffec849c1aa33b4c98f
Vipas12 <avipconta@gmail.com>
2025-02-09 19:38:36 +0000

    43

---
+++ Ex04.08Teste.py
@@ -0,0 +1,11 @@
+print(10%(3*10)**2+(1-10)*4/2)
+numero = 110.9657
+print ("valor inteiro:%d, Valor decimal:%f."%(numero, numero))
+print ("valor arredondado: %3.2f"%numero)
+#v=input("insira um numero\n->")
+#print("valor=", v, "\tquadrado=", int(v)**2)
+a=34
+if a==3:
+    print (a+1)
+else:
+    print (a-1)
