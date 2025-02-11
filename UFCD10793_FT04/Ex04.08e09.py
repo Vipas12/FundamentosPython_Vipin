b483d60f3fe78f7754fd0ab4ab4f525f45d2c1a1
Vipas12 <avipconta@gmail.com>
2025-02-11 22:41:13 +0000

    erew

--- UFCD10793_FT04/Ex04.08e09.py
+++ UFCD10793_FT04/Ex04.08e09.py
@@ -1,6 +1,30 @@
 #Escreve um programa que coloque no ecrã a tabuada do número 5.#
-n=int(input("digite o numero da tabuada pretendida: "))
+'''n=int(input("digite o numero da tabuada pretendida: "))
 i=0
 while i<10:
     i=i+1
     print(n, "x", i, "=", n*i)
+'''
+    
+'''Escreve um programa que coloque no ecrã a tabuada do número 5
+
+print("\nTabuada do número 5:")
+numero1 = 0
+tabuada = 5
+multiplicacao = 5
+while numero1 <= 10:
+    
+    multiplicacao = tabuada * numero1 
+    print(f"{tabuada} x {numero1} = {multiplicacao}")
+    numero1 = numero1 + 1
+'''
+
+tabuada=5
+for i in range(1,11):
+    print(tabuada, "*", i, "=", tabuada * i)
+    
+#opção2 
+numero = int(input("Digite o número para cálcular a tabuada\n---->\t3"))
+for x in range (1,11):
+    multiplicacao = numero * x
+    print (f"{numero}*{x}={multiplicacao}\n")
---
+++ UFCD10793_FT04/Ex04.10.py
@@ -0,0 +1,10 @@
+#Escreve um programa que coloque no ecrã os 10 primeiros números de forma decrescente#
+n=int(input("digite um numero inteiro: "))
+for i in range (n):
+    print(n)
+    n=n-1
+
+'''Escreve um programa que coloque no ecrã os 10 primeiros números de forma decrescente
+for i in range(10, 0, -1):
+    print(i)
+'''
