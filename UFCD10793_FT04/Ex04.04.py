8fc2509e1d9339d75cd53ec86669c240037e5967
Vipas12 <avipconta@gmail.com>
2025-02-11 20:37:13 +0000

    df

--- UFCD10793_FT04/Ex04.04.py
+++ UFCD10793_FT04/Ex04.04.py
@@ -1,11 +1,21 @@
-#Faz um programa que escreva o nome do mês que é introduzido, pelo utilizador, na forma numérica#
-'''print ("Olá "*100)'''
 '''Fazer um programa para ler quatro números inteiros e positivos, calcular e devolver  a  sua 
 média. - usando o ciclo while'''
-contador = 0
+'''contador = 1
 while contador <100:
     contador =contador + 1
     print("Olá")
+'''
+   
+'''Escreve um programa para escrever no ecrã a palavra olá 100 vezes.'''
+#adiciono um contador para guardar o número inicial
+contador = 1
+#enquanto o contador for menor ou igual a 100, escrevo a palavra olá e incremento o contador para aparecer a quantidade de vezes
+while contador <= 100:
+    #imprime os olás
+    print(f"{contador} Olá")
+     #aumenta o contador para avançar para o próximo olá
+    contador += 1   
+   
     
 # x+=1 é o mesmo que x = x + 1
 # x-=1 é o mesmo que x = x - 1
