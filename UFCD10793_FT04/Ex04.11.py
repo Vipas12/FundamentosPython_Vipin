3b13997189fb452b70d925a2e1d7da2b5e08c55b
Vipas12 <avipconta@gmail.com>
2025-02-11 22:48:15 +0000

    er

--- UFCD10793_FT04/Ex04.11.py
+++ UFCD10793_FT04/Ex04.11.py
@@ -1,4 +1,16 @@
-i=1
-while i<=100:
+#Escreve um programa que calcule a soma e o produto dos N primeiros números naturais#
+"""i=1
+while i<=5:
     print(f"{i}\t{i**2}")
     i=i+1
+"""
+y=0
+z=1
+x= input("Introduza um número para calcular a soma: ")
+x= int(x)
+for i in range(1,x+1):
+    y += i
+    z *= i
+    
+print(y)
+print(z)
