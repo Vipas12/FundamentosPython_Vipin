1671aeb68b661574d0e84caf853f552df61993d3
Vipas12 <avipconta@gmail.com>
2025-02-12 22:06:53 +0000

    az

--- UFCD10793_FT04/Ex04.16.py
+++ UFCD10793_FT04/Ex04.16.py
@@ -1,1 +1,11 @@
-#Escreva um programa que peça ao utilizador números entre 0-10. Se o utilizador inserir um númerofora desse intervalo o programa deverá finalizar com uma mensagem personalizada
+#Escreva um programa que peça ao utilizador números entre 0-10.
+# Se o utilizador inserir um número fora desse intervalo o programa
+# deverá finalizar com uma mensagem personalizada
+'''n=int(input("insira um numero entre 0 e 10: "))
+if n>10 or n<0:
+    print("numero inserido não está dentro do intervalo 0 e 10")
+'''
+n=int(input("insira um numero entre 0 e 10: "))
+while n>10 or n<0:
+    print("numero inserido não está dentro do intervalo 0 e 10")
+    break
