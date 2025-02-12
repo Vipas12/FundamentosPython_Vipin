a4666bb73810093773533f314003b1f421bdf7a1
Vipas12 <avipconta@gmail.com>
2025-02-12 22:08:17 +0000

    df

--- UFCD10793_FT04/Ex04.17.py
+++ UFCD10793_FT04/Ex04.17.py
@@ -1,1 +1,2 @@
-#Escreva um programa que peça ao utilizador 20 números reais e no final mostre a soma e a médiados números introduzidos.
+#Escreva um programa que peça ao utilizador 20 números reais e no final
+# mostre a soma e a médiados números introduzidos.
--- UFCD10793_FT04/Ex04.19.py
+++ UFCD10793_FT04/Ex04.19.py
@@ -1,2 +1,2 @@
 #Escreve um programa que solicite ao utilizador um número e escreva em simultâneo
-# a sequênciacrescente e decrescente entre 1 e esse número.
+# a sequência crescente e decrescente entre 1 e esse número.
