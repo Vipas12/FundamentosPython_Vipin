fe8de9ca1daba7fb0d7416e71035aaec957ee771
Vipas12 <avipconta@gmail.com>
2025-02-22 20:34:14 +0000

    df

--- UFCD10793_Avalicao_Interm_2025Fev/P1.05.py
+++ UFCD10793_Avalicao_Interm_2025Fev/P1.05.py
@@ -3,12 +3,12 @@ e some todos os números de 1 até esse mesmo número.
 Deves utilizar o tratamento de erros.'''
 
 try:
-    num=int(input("Digite um número: "))
+    num=int(input("Digite um número inteiro: "))
     soma=0
     for i in range(num+1):
-        soma=soma+i
-    print(soma)
+        soma+= i
+    print(f"A soma dos números de 0 a {num} é: {soma}")
 except ValueError:
-    print("Erro: Digite um número válido.")
+    print("Erro: Digite um número inteiro válido.")
 except Exception as e:
     print("Erro inesperado: ", e)
--- UFCD10793_Avalicao_Interm_2025Fev/P1.06.py
+++ UFCD10793_Avalicao_Interm_2025Fev/P1.06.py
@@ -1,1 +1,13 @@
-
+'''Elabora uma script em python que peça ao utilizador dois números
+e devolva a divisão do primeiro número introduzido pelo seguinte.
+Trate erros como divisão por zero e valores inválidos.'''
+try:
+    numerador=float(input("Digite o numerador: "))
+    denominador=float(input("Digite o denominador: "))
+    print(f"A divisão dos números é {numerador/denominador:.2f}")
+except ValueError:
+    print("Erro: Digite um número válido")
+except ZeroDivisionError:
+    print("Erro: Não é possivel dividir por zero.") 
+except Exception as e:
+    print("Erro inesperado: ", e)
