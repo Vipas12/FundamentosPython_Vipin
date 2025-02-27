64593617f2ced8e3f1aa38d8e209f953d11c01fd
Vipas12 <avipconta@gmail.com>
2025-02-27 22:52:50 +0000

    sf

--- UFCD10793_FT11/Ex11.02.py
+++ UFCD10793_FT11/Ex11.02.py
@@ -1,3 +1,7 @@
-def quadrado (lado)
+def quadrado (lado):
     print ("Área é igual a: ", lado*lado)
-    print ("Perimetro é igual a:" 4*lado")
+    print ("Perimetro é igual a:", 4*lado)
+
+lado=input("Digite o comprimento do lado de um quadrado: ")
+
+print(quadrado)
