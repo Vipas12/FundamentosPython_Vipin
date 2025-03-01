2248e081f759b109d691fa3b9a2e04024fd46257
Vipas12 <avipconta@gmail.com>
2025-03-01 19:40:24 +0000

    srewr

--- UFCD10793_Avalicao_Interm_2025Fev/banco_dados.py
+++ UFCD10793_Avalicao_Interm_2025Fev/banco_dados.py
@@ -6,7 +6,7 @@
 
 import sqlite3
 
-#Criar conexão com o banco de dados (ou criar se não existir)
+#Ex_1 Criar conexão com o banco de dados (ou criar se não existir)
 conn=sqlite3.connect('empresa.db')
 cursor=conn.cursor()
 
@@ -19,23 +19,56 @@ CREATE TABLE IF NOT EXISTS funcionarios (
     salario INTEGER)
 ''')
 
-#Inserir funcionários na tabela
+#Ex_2.1 Inserir funcionários na tabela
 cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)", ('Ana Silva', 'Gestora', 3500))
 cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)", ('Pedro Santos', 'Programador', 3700))
 cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)", ('Mariana Costa', 'Designer', 2000))
 
-#Inserir mais 2 funcionários na tabela
+#Ex_2.2 Inserir mais 2 funcionários na tabela
 cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)", ('Ana Oliveira', 'Gestora Senior', 4500))
 cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)", ('Alexandre Martins', 'Junior', 1500))
 
-
-#consultar todos os funcionarios
+#Ex_3.1 Consultar todos os funcionarios
 cursor.execute("SELECT * FROM funcionarios")
 funcionarios=cursor.fetchall()
 
+#Ex_3.2 Exibir os resultados
+for x in funcionarios:
+    print(x)
+
+#Ex_4.1 Atualizar o salario do Pedro Santos
+cursor.execute("UPDATE funcionarios SET salario = 3000 WHERE nome = 'Pedro Santos'")
+
+cursor.execute("SELECT * FROM funcionarios WHERE nome=?", ('Pedro Santos',))
+funcionario=cursor.fetchone()
+if funcionario:
+  print(funcionario)
+else:
+    print("funcionario não encontrado")  
+
+#Ex_4.2 Aumento salarios em 5%
+cursor.execute("UPDATE funcionarios SET salario = salario*1.05")
 #Exibir os resultados
-for i in funcionarios:
-    print(i)
+cursor.execute("SELECT * FROM funcionarios")
+funcionarios=cursor.fetchall()
+for x in funcionarios:
+    print(x)
+
+#Ex_5.1 Remover 1 funcionario da base de dados
+cursor.execute("DELETE FROM funcionarios WHERE nome = 'Mariana Costa'")
+#Exibir os resultados
+cursor.execute("SELECT * FROM funcionarios")
+funcionarios=cursor.fetchall()
+for x in funcionarios:
+    print(x)
+    
+#Ex_5.2 Remover todos os funcionarios com salario inferior a 3.000 euros
+cursor.execute("DELETE FROM funcionarios WHERE salario < 3000")
+#Exibir os resultados
+cursor.execute("SELECT * FROM funcionarios")
+funcionarios=cursor.fetchall()
+for x in funcionarios:
+    print(x)
 
 
 # fechar a conexão
