d173b7f1e9672a821e8ce107b88f47a8e2b0e1cf
Vipas12 <avipconta@gmail.com>
2025-03-01 18:14:01 +0000

    ads

--- UFCD10793_Avalicao_Interm_2025Fev/banco_dados.py
+++ UFCD10793_Avalicao_Interm_2025Fev/banco_dados.py
@@ -11,12 +11,32 @@ conn=sqlite3.connect('empresa.db')
 cursor=conn.cursor()
 
 #Criar tabela de funcionarios
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Alunos (id INTEGER PRIMARY KEY AUTOINCREMENT,
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS funcionarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
-    idade INTEGER,
-    curso TEXT)
-""")
+    cargo TEXT NOT NULL,
+    salario INTEGER)
+''')
+
+#Inserir funcionários na tabela
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)", ('Ana Silva', 'Gestora', 3500))
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)", ('Pedro Santos', 'Programador', 3700))
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)", ('Mariana Costa', 'Designer', 2000))
+
+#Inserir mais 2 funcionários na tabela
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)", ('Ana Oliveira', 'Gestora Senior', 4500))
+cursor.execute("INSERT INTO funcionarios (nome, cargo, salario) VALUES (?,?,?)", ('Alexandre Martins', 'Junior', 1500))
+
+
+#consultar todos os funcionarios
+cursor.execute("SELECT * FROM funcionarios")
+funcionarios=cursor.fetchall()
+
+#Exibir os resultados
+for i in funcionarios:
+    print(i)
+
 
 # fechar a conexão
 conn.commit()
