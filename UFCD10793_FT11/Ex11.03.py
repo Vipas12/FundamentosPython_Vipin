0a9b0a164a1b201033550a66b842f428c029c6d5
Vipas12 <avipconta@gmail.com>
2025-03-06 21:29:54 +0000

    ewr

---
+++ UFCD10793_FT11/Ex11.03.py
@@ -0,0 +1,17 @@
+def calcula(dados):
+    for i in dados:
+        soma=soma+i
+    
+    media=soma/len(dados)
+  
+entrada=("introduz uma lista de numeros separados por espaço: ")
+
+dados=list(map(int, entrada.split())) 
+
+
+calcula (dados)
+
+print("A soma dos elementos da lista é ", soma )
+print ("a média é ", media) 
+print("o numero de elementos da lista é ", len(dados))
+    
