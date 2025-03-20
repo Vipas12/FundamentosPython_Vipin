1451c6a0e2a0afc6fbd2eaba8b8e549c8fa27c03
Vipas12 <avipconta@gmail.com>
2025-03-20 22:50:00 +0000

    dd

--- UFCD10793_Avaliacao_Final_2025_Marco/Ex2_Av_final.py
+++ UFCD10793_Avaliacao_Final_2025_Marco/Ex2_Av_final.py
@@ -1,7 +1,6 @@
 #Escreve uma função em Python que dada uma palavra retorne o número de vogais nessa palavra.
 
-#palavra=input('Digite 1 palavra: ')
-palavra="gaaaaaaaas"
+palavra=input('Digite 1 palavra: ')
 vogais="aeiouAEIOU"
 soma=0
 for i in vogais:
