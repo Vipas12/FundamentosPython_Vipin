b46ad6051ce84b62be0eb085330e4f4e882423ea
Vipas12 <avipconta@gmail.com>
2025-03-20 21:09:13 +0000

    dfd

---
+++ UFCD10793_Avaliacao_Final_2025_Marco/Ex3_Av_final.py
@@ -0,0 +1,9 @@
+#Escreve uma função em Python que dada uma lista de números imprime 
+# a soma dos valores dessa lista, o número de elementos da lista e a media desses valores.
+# Implementa tratamento de exceções no teu código (try…except…else..finally).
+
+list = [1,4,6,8,45,78,99]
+print(list.soma())
+print(len.list())
+print(average.list())
+
---
+++ UFCD10793_Avaliacao_Final_2025_Marco/Ex4_Av_final.py
@@ -0,0 +1,15 @@
+#4.	Utilizando a biblioteca NumPy :
+#a.	Cria um array de números 1 com shape (8, 2)
+#b.	Cria um array de zeros com shape (5, 7)
+#c.	Subtraia o novo array de outro com dados aleatórios e armazene o numa variável chamada subarray
+#d.	Calcula a média dos valores do array subarray
+
+import numpy as 
+arry1=[1,1,1,1,1,1,1,1
+      1,1,1,1,1,1,1,1]
+
+arry2=[0,0,0,0,0,0
+       0,0,0,0,0,0
+       0,0,0,0,0,0
+       0,0,0,0,0,0
+       0,0,0,,0,0]
