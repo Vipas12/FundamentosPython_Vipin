33fb47892acd03f8a3a25ff2f07daf86556913d2
Vipas12 <avipconta@gmail.com>
2025-03-20 21:00:31 +0000

    df

---
+++ UFCD10793_Avaliacao_Final_2025_Marco/Ex1_Av_final.py
@@ -0,0 +1,7 @@
+#Ex1 Leia uma cadeia de caracteres no formato “DD/MM/AAAA” e 
+#copie o dia, mês e ano para três variáveis inteiras
+
+x=str(input('introduza uma data no formato DD/MM/AAA: '))
+print(x)
+
+
---
+++ UFCD10793_Avaliacao_Final_2025_Marco/Ex2_Av_final.py
@@ -0,0 +1,6 @@
+#Escreve uma função em Python que dada uma palavra retorne o número de vogais nessa palavra.
+
+x=str(input('Digite 1 palavra: '))
+n_vogais=['a','e','i','o','u']
+for in_vogais in x
+    
